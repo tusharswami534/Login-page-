0b81cdeabb98ef6e584e7a26e360cf8fab1b69c8
tusharswami534 <tusharswami17@gmail.com>
2024-07-11 21:23:52 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+const login = () =>{
+    document.getElementById("end").classList.toggle("justify-content-end")
+    document.getElementById("d-b").classList.toggle("d-f")
+    document.getElementById("d-n").classList.toggle("d-n")
+}
